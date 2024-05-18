21c3fcfdb303224ae8eb1b8ed15237db71cbabe4
HiroakiTakami <146831458+takamihiroaki@users.noreply.github.com>
2024-05-18 19:57:43 +0000

    ver.3.7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-
+# バリデーションの設定
   validates :title, presence: true
   validates :introduction, presence: true
   validates :image, presence: true
